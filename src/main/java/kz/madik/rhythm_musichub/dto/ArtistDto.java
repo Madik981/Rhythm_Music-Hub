82c92daee78d60f99a94fdac128501aa86a27a51
Madik981 <madi12312456@gmail.com>
2025-12-14 18:51:01 +0500

    added dto to Artist

---
+++ src/main/java/kz/madik/rhythm_musichub/dto/ArtistDto.java
@@ -0,0 +1,13 @@
+package kz.madik.rhythm_musichub.dto;
+
+public class ArtistDto {
+    private Long id;
+
+    private String nameDto;
+
+    private String nationalityDto;
+
+    private int ageDto;
+
+    private String genderDto;
+}
