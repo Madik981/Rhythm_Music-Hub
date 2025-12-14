ce682f289c631e64816dca5dce31f539dbcff53a
Madik981 <madi12312456@gmail.com>
2025-12-14 17:53:57 +0500

    created project folder

---
+++ src/main/java/kz/madik/rhythm_musichub/RhythmMusicHubApplication.java
@@ -0,0 +1,13 @@
+package kz.madik.rhythm_musichub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhythmMusicHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhythmMusicHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/madik/rhythm_musichub/RhythmMusicHubApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.madik.rhythm_musichub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhythmMusicHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
