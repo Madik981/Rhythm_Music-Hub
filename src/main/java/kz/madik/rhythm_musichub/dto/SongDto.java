da51a6b7578b98ae6ddd6e98d94ea0e5197ebe7d
Madik981 <madi12312456@gmail.com>
2025-12-14 19:48:46 +0500

    added mapper to Song

--- src/main/java/kz/madik/rhythm_musichub/dto/SongDto.java
+++ src/main/java/kz/madik/rhythm_musichub/dto/SongDto.java
@@ -1,9 +1,15 @@
 package kz.madik.rhythm_musichub.dto;
 
+import java.util.List;
+
 public class SongDto {
-    private Long id;
+    private Long idDto;
 
     private String nameDto;
 
     private int durationDto;
+
+    private AlbumDto albumDto;
+
+    private List<ArtistDto> artistsDto;
 }
---
+++ src/main/java/kz/madik/rhythm_musichub/mapper/SongMapper.java
@@ -0,0 +1,28 @@
+package kz.madik.rhythm_musichub.mapper;
+
+import kz.madik.rhythm_musichub.dto.SongDto;
+import kz.madik.rhythm_musichub.entity.Song;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface SongMapper {
+    @Mapping(target = "idDto", source = "id")
+    @Mapping(target = "nameDto", source = "name")
+    @Mapping(target = "durationDto", source = "duration")
+    @Mapping(target = "albumDto", source = "album")
+    @Mapping(target = "artistsDto", source = "artists")
+    SongDto toDto(Song song);
+
+    @Mapping(target = "id", source = "idDto")
+    @Mapping(target = "name", source = "nameDto")
+    @Mapping(target = "duration", source = "durationDto")
+    @Mapping(target = "album", source = "albumDto")
+    @Mapping(target = "artists", source = "artistsDto")
+    Song toEntity(SongDto songDto);
+
+    List<SongDto> toDtoList(List<Song> songs);
+
+}
