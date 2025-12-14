21d4c747bc1f0e1581c0a2d94dddb2daaf2df639
Madik981 <madi12312456@gmail.com>
2025-12-14 20:14:36 +0500

    fixed lack of getter, setter, constructor in dto

--- src/main/java/kz/madik/rhythm_musichub/dto/ArtistDto.java
+++ src/main/java/kz/madik/rhythm_musichub/dto/ArtistDto.java
@@ -1,5 +1,14 @@
 package kz.madik.rhythm_musichub.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ArtistDto {
     private Long idDto;
 
--- src/main/java/kz/madik/rhythm_musichub/dto/SongDto.java
+++ src/main/java/kz/madik/rhythm_musichub/dto/SongDto.java
@@ -1,7 +1,16 @@
 package kz.madik.rhythm_musichub.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SongDto {
     private Long idDto;
 
