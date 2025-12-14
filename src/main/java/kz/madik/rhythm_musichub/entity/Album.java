d64da68b1618cdea769897c8876a348e553d04f2
Madik981 <madi12312456@gmail.com>
2025-12-14 19:33:55 +0500

    added getter, setter, constructor to Entities

--- src/main/java/kz/madik/rhythm_musichub/entity/Album.java
+++ src/main/java/kz/madik/rhythm_musichub/entity/Album.java
@@ -1,8 +1,16 @@
 package kz.madik.rhythm_musichub.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "t_album")
 public class Album {
     @Id
--- src/main/java/kz/madik/rhythm_musichub/entity/Song.java
+++ src/main/java/kz/madik/rhythm_musichub/entity/Song.java
@@ -1,10 +1,18 @@
 package kz.madik.rhythm_musichub.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "t_song")
 public class Song {
     @Id
