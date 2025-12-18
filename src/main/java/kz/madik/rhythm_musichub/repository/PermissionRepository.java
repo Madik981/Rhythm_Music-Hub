41a1212e1aaaec4015b6cdbb089768a9c4c8f0f4
Madik981 <madi12312456@gmail.com>
2025-12-18 13:07:23 +0500

    ADD. added models and repo for Auth

---
+++ src/main/java/kz/madik/rhythm_musichub/repository/PermissionRepository.java
@@ -0,0 +1,10 @@
+package kz.madik.rhythm_musichub.repository;
+
+import kz.madik.rhythm_musichub.entity.Permission;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PermissionRepository extends JpaRepository<Permission, Long> {
+    Permission findByName(String name);
+}
