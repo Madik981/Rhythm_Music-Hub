73f9c8808ed60a778035e4df39b7e124c667b324
Madik981 <madi12312456@gmail.com>
2025-12-18 13:12:35 +0500

    ADD. added service and config for Auth

---
+++ src/main/java/kz/madik/rhythm_musichub/service/MyUserService.java
@@ -0,0 +1,53 @@
+package kz.madik.rhythm_musichub.service;
+
+import kz.madik.rhythm_musichub.entity.Permission;
+import kz.madik.rhythm_musichub.entity.UserModel;
+import kz.madik.rhythm_musichub.repository.PermissionRepository;
+import kz.madik.rhythm_musichub.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class MyUserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private PermissionRepository permissionRep;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserModel user = userRepository.findByEmail(username);
+
+        if(Objects.nonNull(user)) {
+            return user;
+        }
+        throw new UsernameNotFoundException("User Not Found");
+    }
+
+    public void register(UserModel model){
+        UserModel check = userRepository.findByEmail(model.getEmail());
+        if (check == null){
+            model.setPassword(passwordEncoder.encode(model.getPassword()));
+            List<Permission> permissions = List.of(permissionRep.findByName("ROLE_USER"));
+
+            model.setPermissions(permissions);
+            userRepository.save(model);
+        }
+    }
+
+
+}
